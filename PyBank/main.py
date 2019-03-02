e88415526e3677f9bc3da313ffd8b057ecce0c88
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-03-02 14:37:12 -0500

    WIP: 🚧  Changed the total_profit to absolute, revenue_change moved to the outside of the for loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,4 @@
-
-  # The net total amount of "Profit/Losses" over the entire period
-
-  # The average of the changes in "Profit/Losses" over the entire period
+  # The average of the changes in months in "Profit/Losses" over the entire period
 
   # The greatest increase in profits (date and amount) over the entire period
 
@@ -13,38 +10,34 @@ import csv
 
 total_months = 0
 total_profit = 0
+revenue_change = 0
+revenue_change_list = []
+month_of_change = []
 
 csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     for row in csvreader: 
-        data = list(csvreader)
-        row_count = len(data)
+        total_months = total_months + 1
         #total_profit = sum(int(row[1]) for row in csv.reader(csvfile))
-        total_profit = total_profit + int(row[1])
+        total_profit = total_profit + abs(int(row[1]))
+        
+    revenue_change = total_profit / total_months
 
-#def get_averages(csvpath):
-    #column_sums = None
-    #with open(csvpath) as file:
-        #lines = file.readlines()
-        #rows_of_numbers = [map(float, line.split(',')) for line in lines]
-        #sums = map(sum, zip(*rows_of_numbers))
-        #averages = [sum_item / len(lines) for sum_item in sums]
-        #return averages
-
-#print(get_averages(csvpath))
-
+        # Take the difference between two months and append to monthly profit change
+        #monthly_profit_change = total_profit[row+1] - total_profit[row]
+        #previous_revenue = float(row[1])
+        #revenue_change = float(row[1])- previous_revenue
+        #revenue_change_list = revenue_change_list + [revenue_change]
+        #month_of_change = [month_of_change] + [row[0]]
+        
 
 
 
-
-
-
-
-print("Total Months: " + str(row_count))  
+print("Total Months: " + str(total_months))  
 print("Total Profit: " + str(total_profit))
-
+print("Average Change: " + str(revenue_change))
 
 
   #Total: $38382578
