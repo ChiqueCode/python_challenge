706793ae3084655022dc4674b88bf9958d6a6d57
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-03-05 13:20:11 -0500

    WIP: 🚧  Found the winner for PyPoll and percentage of votes for the winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,3 @@
-#The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote.
-
 import os
 import csv
 
@@ -12,6 +6,10 @@ candidate_options = []
 candidates_votes = {}
 candidate_winner = ""
 winning_count = 0
+percentage = []
+count_candidates = 0
+sum = 0
+
 
 
 pypoll = os.path.join("Resources", "election_data.csv")
@@ -21,47 +19,34 @@ with open(pypoll, 'r') as csvfile:
     for row in csvreader:
         #calculating total votes
         total_votes = total_votes + 1
-        # adding every candidate to the list
-        #output.append(row[2]) 
+        # dictionary for each candidate and num of votes
         candidate_name = row[2]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidates_votes[candidate_name] = 0
-        candidates_votes[candidate_name] += 1
-    print(candidates_votes)
-        #if row[2] not in candidates:
-            #candidates.append(row[2])
-            #index = candidates.index(row[2])
-            #num_votes.append(1)
-        #else:
-            #index = candidates.index(row[2])
-            #num_votes[index] += 1
+        candidates_votes[candidate_name] += 1 
 
-# looping through the rows to find unique values = candidates
-    #f#or x in output:
-        #if x not in output:
-            #unique.append(x)
-   # print(unique)            
+    #finding the percentage for each candidate's votes        
+    for (key, value) in candidates_votes.items(): sum += value
+    percentage = candidates_votes['Khan'] / (sum + 0.0)
+    #percentage = candidates_votes['O'Tooley'] / (sum + 0.0)
+    #for (key, value) in candidates_votes.items(): sum += value
+    #percentage = candidates_votes['Correy'] / (sum + 0.0)
+    #percentage = candidates_votes['Li'] / (sum + 0.0)
+    #print "percentage of '%s' is %.2f %%" % ('a' , percentage*100)
     
 print("Total Votes: " + str(total_votes))
-#print(candidates)
-
-
-        
-
-
-
+print("Candidates are: " + str(candidates_votes))
+print "Percentage of '%s' is %.2f %%" % ('Votes for Khan' , percentage*100) 
+#print "Percentage of '%s' is %.2f %%" % ('Votes for Correy' , percentage*100) 
+#print "Percentage of '%s' is %.2f %%" % ('Votes for Li' , percentage*100) 
 
 
 
 
 
+print("The Winner is: " + max(candidates_votes, key=candidates_votes.get))
 
-
-#Election Results
-#-------------------------
-#otal Votes: 3521001
-#-------------------------
 #Khan: 63.000% (2218231)
 #Correy: 20.000% (704200)
 #Li: 14.000% (492940)
